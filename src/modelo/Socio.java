a46c4123b6505fb9c2a7afab14ba40734d2e461c
UCLPARKING <UCLPARKING@UCLPARKING>
2016-05-20 21:00:43 +0200

    Clase Ventana Principal terminada

--- src/modelo/Socio.java
+++ src/modelo/Socio.java
@@ -6,7 +6,46 @@ package modelo;
  * @version 20/05/2016
  */
 public class Socio {
-    private int cod_soc;
+    private String cod_soc;
+    private String nombre;
+    private String apellido;
+    private String dni_nif;
+    private String direccion;
+    private String codPostal;
+    private String provincia;
+    private String localidad;
+
+    /**
+     * 
+     * @param cod_soc Número que representa el codigo
+     * @param nombre String con el nombre
+     * @param apellido String con los apellidos
+     * @param dni_nif String con el DNI o NIF
+     * @param direccion String con la direccion
+     * @param codPostal Numero que representa el código postal
+     * @param provincia String con la provincia
+     * @param localidad String con la localidad
+     * @see #compruebaDatos() 
+     */    
+    public Socio(String cod_soc, String nombre, String apellido, String dni_nif, 
+                 String direccion, String codPostal, String provincia, String localidad) {
+        this.cod_soc = cod_soc;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni_nif = dni_nif;
+        this.direccion = direccion;
+        this.codPostal = codPostal;
+        this.provincia = provincia;
+        this.localidad = localidad;
+        compruebaDatos();        
+    }
+    
+    /**
+     * comprueba y corri
+     */
+    private void compruebaDatos(){
+        
+    }
     
     
     
--- src/vista/VentanaPpal.java
+++ src/vista/VentanaPpal.java
@@ -5,10 +5,147 @@
  */
 package vista;
 
+import controlador.CtrlDataBase;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
 /**
  *
  * @author Mario
  */
-public class VentanaPpal {
+public class VentanaPpal extends JFrame implements ActionListener,WindowListener {
+    JPanel contenedor;
+    JButton [] boton;
+    JLabel titulo;
+    CtrlDataBase db;
+    
+    public VentanaPpal(CtrlDataBase db){
+        this.db=db;
+        this.setTitle("Club Social Nosferatus");
+        this.setVisible(true);
+        initComponents();
+    }
+    
+    public JComponent menu(){
+        String [] opciones={"Alta socio","Baja socio",
+                            "Modificacion Socio","Crear XML",
+                            "Listados","Fin"};
+        JPanel inner = new JPanel();
+        inner.setLayout(new GridLayout(opciones.length,1,0,10));
+        
+        boton=new JButton [opciones.length];
+        for(int x=0;x<boton.length;x++){
+            boton[x]=new JButton();
+            inner.add(boton[x]);
+            boton[x].setText(opciones[x]);
+            boton[x].addActionListener(this);  
+            boton[x].setActionCommand(""+(x+1));    
+        }
+        return inner;
+    }
+    
+    private void initComponents(){
+       
+        contenedor=(JPanel) this.getContentPane();
+
+        contenedor.setLayout(new BorderLayout());
+        titulo=new JLabel("Curso",SwingConstants.CENTER);
+        titulo.setFont(new Font("Courier", Font.BOLD, 24));
+         
+        
+
+        contenedor.add(titulo,BorderLayout.NORTH);
+        contenedor.add(menu(),BorderLayout.CENTER);
+        contenedor.add(new JLabel("  .  "),BorderLayout.EAST);
+        contenedor.add(new JLabel("  .  "),BorderLayout.WEST);
+        contenedor.add(new JLabel("Mario L. 1º DAM",SwingConstants.CENTER),BorderLayout.SOUTH);
+            
+        this.pack();
+        this.setSize(300,300);
+        
+    }
+    
+    
+    /**
+     * ToDo
+     */
+    private void fin(){
+        //DocumentoXML.generaXML("Clase", db.ejecutaConsulta("SELECT * from alumnos"));
+        //db.cerrarConexion();
+        System.exit(0);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()){
+            case "1":
+                //VentanaAlta va=new VentanaAlta(db,this);
+                break;
+            case "2":
+                //VentanaBaja vb=new VentanaBaja(db,this);
+                break;
+            case "3":
+                //VentanaListado vl=new VentanaListado(db.ejecutaConsulta("SELECT * from alumnos"));
+                break;                
+            case "4":
+                //VentanaPNota vp=new VentanaPNota(db,this);
+                break;
+            case "5":
+                //VentanaBorrarSus va=new VentanaBorrarSus(db,this);
+                break;
+            case "6":
+                //fin();
+                break;                
+            default: 
+                this.dispose();
+                break;
+        }
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        //fin();
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        //fin();
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
